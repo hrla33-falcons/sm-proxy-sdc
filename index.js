b0bb2447118896eb394dc4724bec8a59e265a96b
Steve McLaughlin <stevejrmc@gmail.com>
2020-01-11 12:09:03 -0800

    Add loaderio verification route

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ let options = {
     '54.193.48.102:3000/reviews': 'http://52.53.155.153:3003/',
     '54.193.48.102:3000/ikea': 'http://localhost:3007/',
     '54.193.48.102:3000/products': 'http://localhost:3001/',
-    '54.193.48.102:3000/searchbar': 'http://localhost:3002/'
+    '54.193.48.102:3000/searchbar': 'http://localhost:3002/',
+    '54.193.48.102:3000/loaderio-84f46b8a0bcf09f289e60658c7a7f157': 'http://52.53.155.153:3003/'
   }
 }
 let ikeaProxy = proxy(options)
