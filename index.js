2c30e0e4a0ebcdc4970bddf2f2d920903aac7e1a
Steve McLaughlin <stevejrmc@gmail.com>
2020-01-11 10:49:51 -0800

    Update ip addresses on the server

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const port = 3000
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(morgan('dev'))
 let options = {
-  target: 'http://localhost:3000',
+  target: 'http://54.193.48.102:3000',
   router: {
-    'localhost:3000/reviews': 'http://localhost:3003/',
-    'localhost:3000/ikea': 'http://localhost:3007/',
-    'localhost:3000/products': 'http://localhost:3001/',
-    'localhost:3000/searchbar': 'http://localhost:3002/'
+    '54.193.48.102:3000/reviews': 'http://52.53.155.153:3003/',
+    '54.193.48.102:3000/ikea': 'http://localhost:3007/',
+    '54.193.48.102:3000/products': 'http://localhost:3001/',
+    '54.193.48.102:3000/searchbar': 'http://localhost:3002/'
   }
 }
 let ikeaProxy = proxy(options)
